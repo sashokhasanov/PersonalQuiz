bf9fefb63a2f91a5883ff154db0b94e2b3aec99b
Aleksandr Khasanov <sashokhasanov@mail.ru>
2021-12-20 23:33:38 +0300

    refactoring

--- PersonalQuiz/ViewControllers/ResultsViewController.swift
+++ PersonalQuiz/ViewControllers/ResultsViewController.swift
@@ -9,20 +9,22 @@ import UIKit
 
 class ResultsViewController: UIViewController {
     
-    var answers: [Answer]!
-    
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    var answers: [Answer]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let result = getAnimalType()
-        resultLabel.text = "Вы - \(result.rawValue)!"
-        descriptionLabel.text = result.definition
+        self.navigationItem.setHidesBackButton(true, animated: false)
+        
+        let animalType = getResultAnimalType()
+        resultLabel.text = "Вы - \(animalType.rawValue)!"
+        descriptionLabel.text = animalType.definition
     }
 
-    private func getAnimalType() -> AnimalType {
+    private func getResultAnimalType() -> AnimalType {
         let results = answers.reduce(into: [:]) { result, answer in result[answer.type, default: 0] += 1 }
         return results.sorted(by: { $0.value > $1.value }).first?.key ?? .dog
     }
